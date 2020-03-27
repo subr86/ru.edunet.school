82dce841918c3b15ea4d32fbbadff5b28abbcea2
root <root@ip-172-31-11-192.eu-central-1.compute.internal>
2020-03-27 06:17:30 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'edunetru';
+$CFG->dbuser    = 'edunetru';
+$CFG->dbpass    = 'aGbY0WSwq97QEhHV';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://ru.edunet.school';
+$CFG->dataroot  = '/var/www/moodledata_ru';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
